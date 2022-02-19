a854226ddc4be98db55a3f5c675ef00d891b9e48
iamharshtita <77393630+iamharshtita@users.noreply.github.com>
2022-02-19 10:16:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+'use strict';
+searchButton.addEventListener('click', searchWeather);
+
+function searchWeather(){
+    var cityName=searchCity.value;
+    if (cityName.trim().length==0){
+        return alert('Please input the City Name');
+    }
+    loadingText.style.display='block';
+    weatherBox.style.display='none';
+   
+    var http= new XMLHttpRequest();
+    var apikey='c566352bfcaa937d72558a18644b6cfd';
+    var url='http://api.openweathermap.org/data/2.5/weather?q='+ cityName + '&units=metric&appid=' + apikey;
+    var method='GET';
+
+    http.open(method, url);
+
+    http.onreadystatechange=function(){
+        if(http.readyState==XMLHttpRequest.DONE && http.status===200){
+            var data=JSON.parse(http.responseText);
+            var weatherData= new weather(cityName.toUpperCase(), data.weather[0].description.toUpperCase());
+            weatherData.temperature=data.main.temp;
+            updateWeather(weatherData);
+        }
+        else if(http.readyState===XMLHttpRequest.DONE){
+             alert('Something went wrong!');
+        }
+    };
+    http.send();
+}
+
+function updateWeather(weatherData){
+    weatherCity.textContent=weatherData.cityName;
+    weatherDescription.textContent=weatherData.description;
+    weatherTemperature.textContent=weatherData.temperature;
+
+    loadingText.style.display='none';
+    weatherBox.style.display='block';
+}
